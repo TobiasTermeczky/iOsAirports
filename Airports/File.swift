001caeecad21a32f2c61ddc83e61f852a1af0f40
Tobias Termeczky@hotmail.com <tobias@Tobiass-Mac.local>
2017-10-17 15:59:53 +0200

    Added Airport Obj

---
+++ Airports/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Airports
+//
+//  Created by Tobias Termeczky@hotmail.com on 17/10/2017.
+//  Copyright © 2017 Tobias Termeczky@hotmail.com. All rights reserved.
+//
+
+import Foundation
---
+++ Airports/Model/Airport.swift
@@ -0,0 +1,19 @@
+//
+//  Airport.swift
+//  Airports
+//
+//  Created by Tobias Termeczky@hotmail.com on 17/10/2017.
+//  Copyright © 2017 Tobias Termeczky@hotmail.com. All rights reserved.
+//
+
+import UIKit
+
+class Airport {
+    var icao = String()
+    var name = String()
+    var latitude = Double()
+    var longitude = Double()
+    var elevation = Double()
+    var iso_country = String()
+    var municipality = String()
+}
