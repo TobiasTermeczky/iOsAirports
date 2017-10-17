f6174a109a449635bd28299c93fb23a421aa2937
Tobias Termeczky@hotmail.com <tobias@Tobiass-Mac.local>
2017-10-17 15:06:29 +0200

    Initial Commit

---
+++ Airports/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Airports
+//
+//  Created by Tobias Termeczky@hotmail.com on 17/10/2017.
+//  Copyright © 2017 Tobias Termeczky@hotmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
